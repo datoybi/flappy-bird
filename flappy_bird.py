865c1f8dd3ae1a7799540c218fe6bc4848025486
dasom yun <dsy0302@gmail.com>
2022-01-01 12:33:00 +0900

    Scrolling pipes

--- flappy_bird.py
+++ flappy_bird.py
@@ -1,12 +1,12 @@
 # py -m pip install -U pygame --user
 import pygame 
 from pygame.locals import *
+import random
 
 pygame.init()
 
 clock = pygame.time.Clock()
 fps = 60
-
 screen_width = 864 
 screen_height = 936
 
@@ -18,6 +18,9 @@ ground_scroll = 0
 scroll_speed = 4
 flying = False
 game_over = False
+pipe_gap = 150
+pipe_frequency = 1500 # milliseconds
+last_pipe = pygame.time.get_ticks() - pipe_frequency
 
 # load images
 bg = pygame.image.load('images/bg.png')
@@ -71,8 +74,25 @@ class Bird(pygame.sprite.Sprite):
         else: # 게임오버면
             self.image = pygame.transform.rotate(self.images[self.index], -90) # -90로테이트 하면 새 얼굴이 땅을 바라보게끔 효과
 
+class Pipe(pygame.sprite.Sprite):
+    def __init__(self, x, y, position):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('images/pipe.png')
+        self.rect = self.image.get_rect()
+        # position 1 is from the top, -1 is from the bottom
+        if position == 1:
+            self.image = pygame.transform.flip(self.image, False, True) # 파이프 flip하기 - x좌표는 false, y좌표만 flip
+            self.rect.bottomleft = [x, y - int(pipe_gap / 2)]
+        if position == -1:
+            self.rect.topleft = [x, y + int(pipe_gap / 2)]
+
+    def update(self):
+        self.rect.x -= scroll_speed
+        if self.rect.right < 0:
+            self.kill()
 
 bird_group = pygame.sprite.Group()
+pipe_group = pygame.sprite.Group()
 flappy = Bird(100, int(screen_height / 2)) 
 bird_group.add(flappy)
 
@@ -84,16 +104,31 @@ while run:
     screen.blit(bg, (0,0)) # 백그라운드 그리기
     bird_group.draw(screen)
     bird_group.update() 
+    pipe_group.draw(screen)
     screen.blit(ground_img, (ground_scroll, 768)) # 땅 그리기
 
-    if flappy.rect.bottom > 768: # 새가 땅에 닿았는지 체크
+    # look for collision
+    if pygame.sprite.groupcollide(bird_group, pipe_group, False, False) or flappy.rect.top < 0: # 파이프에 닿으면 게임오바
+        game_over = True
+
+    if flappy.rect.bottom >= 768: # 새가 땅에 닿았는지 체크
         game_over = True
         flying = False
 
-    if game_over == False:  
+    if game_over == False and flying == True: 
+        time_now = pygame.time.get_ticks() # 새로운 파이프 생성 시간 계산
+        if time_now - last_pipe > pipe_frequency:
+            pipe_height = random.randint(-100, 100)
+            btm_pipe = Pipe(screen_width, int(screen_height / 2) + pipe_height, -1)
+            top_pipe = Pipe(screen_width, int(screen_height / 2) + pipe_height, 1)
+            pipe_group.add(btm_pipe)
+            pipe_group.add(top_pipe)
+            last_pipe = time_now
+
         ground_scroll -= scroll_speed
         if abs(ground_scroll) > 35: # 땅 이미지 반복되게 하기위해
             ground_scroll = 0
+        pipe_group.update()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT: # 창의 x를 누르면
