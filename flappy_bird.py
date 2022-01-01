c07e35b5740cd8d2f926412865f57246a36598a2
dasom yun <dsy0302@gmail.com>
2022-01-01 13:14:13 +0900

    Making reset button

--- flappy_bird.py
+++ flappy_bird.py
@@ -33,10 +33,18 @@ pass_pipe = False
 # load images
 bg = pygame.image.load('images/bg.png')
 ground_img = pygame.image.load('images/ground.png')
+button_img = pygame.image.load('images/restart.png')
 
 def draw_text(text, font, text_col, x, y):
     img = font.render(text, True, text_col)
     screen.blit(img, (x, y))
+
+def reset_game():
+    pipe_group.empty()
+    flappy.rect.x = 100
+    flappy.rect.y = int(screen_height / 2)
+    score = 0
+    return score
 
 class Bird(pygame.sprite.Sprite):
     def __init__(self, x, y):
@@ -103,10 +111,34 @@ class Pipe(pygame.sprite.Sprite):
         if self.rect.right < 0:
             self.kill()
 
+class Button():
+    def __init__(self, x, y, image):
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+
+    def draw(self):
+        action = False
+
+        # get mouse position
+        pos = pygame.mouse.get_pos()
+
+        # check if mouse is over the button
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1:
+                action = True
+
+        # draw button
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+        return action
+
 bird_group = pygame.sprite.Group()
 pipe_group = pygame.sprite.Group()
 flappy = Bird(100, int(screen_height / 2)) 
 bird_group.add(flappy)
+
+# create restart button instance
+button = Button(screen_width // 2 - 50, screen_height // 2 - 100, button_img)
 
 # loop 설정
 run = True
@@ -154,7 +186,14 @@ while run:
         if abs(ground_scroll) > 35: # 땅 이미지 반복되게 하기위해
             ground_scroll = 0
         pipe_group.update()
-
+        
+    # check for game over and reset
+    if game_over == True:
+        if button.draw() == True:
+            game_over = False
+            # print("clicked")
+            score = reset_game()
+            
     for event in pygame.event.get():
         if event.type == pygame.QUIT: # 창의 x를 누르면
             run = False
